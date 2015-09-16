c824aa9f41eac921513b984a9f9b9a2d220f8afd
Chris Knight <cknightdevelopment@gmail.com>
2015-09-16 15:22:49 -0700

    Updated assembly info

--- TableTranslator/Properties/AssemblyInfo.cs
+++ TableTranslator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TableTranslator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET library for flexibly and powerfully translating lists to and from data tables")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chris Knight Development LLC")]
 [assembly: AssemblyProduct("TableTranslator")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Chris Knight Development LLC 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
